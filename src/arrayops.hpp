ab12d101c6b3aba3bcfeb5cc4cc22444ebe729d9
Luke McCulloch <tlukemcculloch@gmail.com>
2019-06-01 23:08:55 -0500

    experimenting with adding a vector class

--- src/arrayops.hpp
+++ src/arrayops.hpp
@@ -95,6 +95,8 @@ matmul(const Array2D<T>& a, const Array2D<T>& b) {
     return c;
 }
 
+
+
 // matmul of two Array2D into a third
 /**
     this matmul over-writes Array2D c
@@ -136,6 +138,46 @@ matmul(const Array2D<T>& a, const Array2D<T>& b, Array2D<T>& c) {
 // addition of Array2D and scalar
 //...
 
+
+
+// Dot of two Array2Ds
+// this matmul creates a new Array2D, c.
+template <class T>
+Array2D<T> 
+Dot(const Array2D<T>& a, const Array2D<T>& b) {
+	assert(a.ncols == b.nrows);
+    Array2D<T> c(a.nrows,b.ncols);
+    c = 0.;
+    for( size_t i=0; i<a.nrows; ++i ) {
+        for( size_t k=0; k<b.ncols; ++k ) {
+            c(i,k) = a(i,0) * b(0,k);
+        }
+        for( size_t j=1; j<a.ncols; ++j ) {
+            for( size_t  k=0; k<b.ncols; ++k ) {
+                c(i,k) += a(i,j) * b(j,k);
+            }
+        }
+    }
+    return c;
+}
+
+// Cross two 1D Array2Ds
+// this matmul creates a new 1D Array2D, c.
+template <class T>
+Array2D<T> 
+Cross(const Array2D<T>& a, const Array2D<T>& b) {
+	assert(a.ncols == b.nrows);
+	assert(a.ncols == 1);
+	assert(b.nrows == 1);
+
+    Array2D<T> c(a.nrows,1);
+
+    c(0,0) =   a(1,0)*b(2,0) - a(2,0)*b(1,0);
+    c(1,0) = -(a(0,0)*b(2,0) - a(2,0)*b(0,0));
+    c(2,0) =   a(0,0)*b(1,0) - a(1,0)*b(0,0);
+
+    return c;
+}
 
 
 
